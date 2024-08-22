abcaaa3f360955045675b9d1fb16c0aa4ee84fe1
ngruenwald <40895194+ngruenwald@users.noreply.github.com>
2024-08-22 21:15:06 +0200

    amalgamate: glob and exclude mode

--- code/_amalgamate.py
+++ code/_amalgamate.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
 
+import glob
 import regex
 
 INPUT_PATH = "code/"
 
 INPUT_FILES = [
-    f"{INPUT_PATH}main.py",
-    f"{INPUT_PATH}command_check.py",
-    f"{INPUT_PATH}command_create.py",
-    f"{INPUT_PATH}command_install.py",
-    f"{INPUT_PATH}load_data.py",
-    f"{INPUT_PATH}package_types.py",
-    f"{INPUT_PATH}resolve_vars.py",
-    f"{INPUT_PATH}set_token.py"
+   f"{INPUT_PATH}*.py"
 ]
+
+GLOB_RECURSIVE = True
 
 TEMPLATE_FILE = f"{INPUT_PATH}clitools.tpl.py"
 
@@ -21,10 +17,16 @@ OUTPUT_FILE = "clitools.py"
 
 INPUT_DATA = {}
 
+EXCLUDE = False
 
-def extract_data_from_file(input_file: str, data: dict) -> None:
-    match_beg = regex.compile("^\\s*#\\s*>>\\s*(.*)$")
-    match_end = regex.compile("^\\s*#\\s*<<\\s*(.*)$")
+BLOCK_BEGIN = regex.compile("^\\s*#\\s*>>\\s*(.*)$")
+BLOCK_END = regex.compile("^\\s*#\\s*<<\\s*(.*)$")
+
+COMMENT_FILE = "# source: {}\n"
+COMMENT_FILE_LINE = "# source: {}:{}\n"
+
+
+def extract_data_from_file_include(input_file: str, data: dict) -> None:
     with open(input_file, "rt") as f:
         block_name: str = ""
         block_data: list[str] = []
@@ -38,7 +40,7 @@ def extract_data_from_file(input_file: str, data: dict) -> None:
             line_number += 1
             if block_name:
                 # inside a block, check for blockend
-                m = match_end.match(line)
+                m = BLOCK_END.match(line)
                 if m:
                     # TBD: should we verify the block name?
                     if block_name in data:
@@ -51,15 +53,56 @@ def extract_data_from_file(input_file: str, data: dict) -> None:
                     block_data.append(line)
             else:
                 # outside of block, check for block start
-                m = match_beg.match(line)
+                m = BLOCK_BEGIN.match(line)
                 if m:
                     block_name = m.group(1).strip()
-                    block_data = [f"# source: {input_file}:{line_number}\n"]
+                    block_data = [COMMENT_FILE_LINE.format(input_file, line_number)]
+
+
+def extract_data_from_file_exclude(input_file: str, data: dict) -> None:
+    with open(input_file, "rt") as f:
+        block_data: list[str] = []
+        line_number: int = 0
+        include_line = True
+        while True:
+            line = f.readline()
+            if not line:
+                if not include_line:
+                    print(f"WARN: eol while in block")
+                break
+            line_number += 1
+            if include_line:
+                # inside of block, check for block end
+                m = BLOCK_END.match(line)
+                if m:
+                    include_line = False
+            else:
+                # outside a block, check for block start
+                m = BLOCK_BEGIN.match(line)
+                if m:
+                    include_line = True
+                else:
+                    block_data.append(line)
+        if block_data:
+            data[""].append("\n")
+            data[""].append(COMMENT_FILE.format(input_file))
+            data[""].extend(block_data)
+
+
+def extract_data_from_file(input_file: str, data: dict) -> None:
+    print(f"processing '{input_file}'")
+    if EXCLUDE:
+        extract_data_from_file_exclude(input_file, data)
+    else:
+        extract_data_from_file_include(input_file, data)
 
 
 def gather_input_data(input_files: list[str], data: dict) -> None:
-    for file in input_files:
-        extract_data_from_file(file, data)
+    for input_file in input_files:
+        files = glob.glob(input_file, recursive=GLOB_RECURSIVE)
+        files.sort()
+        for file in files:
+            extract_data_from_file(file, data)
 
 
 def create_output_file(output_file: str, template_file: str, data: dict) -> None:
