ce098b461543501fb7b2db4632d3ac7fab3a1446
ngruenwald <40895194+ngruenwald@users.noreply.github.com>
2026-08-27 12:22:03 +0200

    feat: add option to accept all updates
    If the check command is run without the '--yes' flag, the user
    had to sign off every change with 'yes' or 'no'.
    This adds an 'all' answer that will auto-accept all following changes.

--- code/ask_user.py
+++ code/ask_user.py
@@ -1,11 +1,26 @@
+# >> imports
+from enum import Enum
+# << imports
+
 # >> ask_user_code
-def ask_user(question: str) -> bool:
+class AskUserResult(Enum):
+    YES = 1
+    NO = 2
+    ALL = 3
+
+def ask_user(question: str, default: AskUserResult = AskUserResult.YES, include_all: bool = True) -> AskUserResult:
+    text_yes: str = "Y" if default == AskUserResult.YES else "y"
+    text_no: str = "N" if default == AskUserResult.NO else "n"
+    text_all: str = "/a" if include_all else ""
+    text_input: str = f"{question} [{text_yes}/{text_no}{text_all}] "
     while True:
-        inp = input(f"{question} [Y/n] ").lower()
+        inp = input(text_input).lower()
         if inp == "":
-            return True
+            return default
         if inp == "y":
-            return True
+            return AskUserResult.YES
         if inp == "n":
-            return False
+            return AskUserResult.NO
+        if include_all and inp == "a":
+            return AskUserResult.ALL
 # << ask_user_code
--- code/command_check.py
+++ code/command_check.py
@@ -1,11 +1,5 @@
 import argparse
 
-from package_types import Package
-from set_token import get_token
-from ask_user import ask_user
-from create_commit import Change, create_commit, push_commit
-from update_package import update_package
-
 # >> imports
 import re
 import requests
@@ -15,40 +9,65 @@ from functools import cmp_to_key
 from time import sleep
 # << imports
 
+from ask_user import ask_user
+from create_commit import Change, create_commit, push_commit
+from package_types import Package
+from set_token import get_token
+from update_package import update_package
+
+
 # >> globals
 CHECK_SESSION: requests.Session | None = None
 # << globals
 
+
 # >> command_check_code
-def create_command_check(subparsers: argparse._SubParsersAction) -> argparse.ArgumentParser:
+def create_command_check(
+    subparsers: argparse._SubParsersAction,
+) -> argparse.ArgumentParser:
     parser = subparsers.add_parser("check", help="check for updates")
     parser.add_argument("--update", action="store_true", help="update package files")
     parser.add_argument("--yes", action="store_true", help="assume yes")
     parser.add_argument("--commit", action="store_true", help="commit changes")
     parser.add_argument("--push", action="store_true", help="push changes")
-    parser.add_argument("--error", action="store_true", help="error rc if no updates are found")
+    parser.add_argument(
+        "--error", action="store_true", help="error rc if no updates are found"
+    )
     parser.set_defaults(func=command_check)
     return parser
 
 
 def command_check(packages: list[Package], args) -> int:
-
     class CheckPackageInfo:
-        def __init__(self, host: str, owner: str, repo: str, version: str, pattern: str | None, no_warn: bool):
+        def __init__(
+            self,
+            host: str,
+            owner: str,
+            repo: str,
+            version: str,
+            pattern: str | None,
+            no_warn: bool,
+        ):
             self.host = host
             self.owner = owner
             self.repo = repo
             self.version = version
             self.pattern = pattern if pattern else r"^v?\d+\.\d+\.\d+$"
             self.no_warn = no_warn
-            self.max_len = None
+            self.max_len: int | None = None
 
         @staticmethod
-        def create(url: str, version: str, pattern: str | None, no_warn: bool) -> "CheckPackageInfo":
+        def create(
+            url: str, version: str, pattern: str | None, no_warn: bool
+        ) -> "CheckPackageInfo":
             from urllib.parse import urlparse
+
             m = urlparse(url)
-            owner, repo = CheckPackageInfo.split_path(m.hostname, m.path)    # owner + repo = github specific?
-            return CheckPackageInfo(m.hostname, owner, repo, version, pattern, no_warn)
+            hostname = m.hostname if m.hostname else ""
+            owner, repo = CheckPackageInfo.split_path(
+                hostname, m.path
+            )  # owner + repo = github specific?
+            return CheckPackageInfo(hostname, owner, repo, version, pattern, no_warn)
 
         @staticmethod
         def split_path(hostname: str, path: str) -> tuple[str, str]:
@@ -56,10 +75,9 @@ def command_check(packages: list[Package], args) -> int:
             # path identifies the owner, and the second one the repository
             parts = path.split("/")
             if len(parts) == 1:
-                return "", parts[0]     # no owner, just repo
+                return "", parts[0]  # no owner, just repo
             parts = [p for p in parts if p]
             return parts[0], parts[1]
-
 
     def get_session() -> requests.Session:
         global CHECK_SESSION
@@ -67,8 +85,7 @@ def command_check(packages: list[Package], args) -> int:
             CHECK_SESSION = requests.Session()
         return CHECK_SESSION
 
-
-    def check_for_updates(pi: CheckPackageInfo) -> str|None:
+    def check_for_updates(pi: CheckPackageInfo) -> str | None:
         package = f"{pi.owner}/{pi.repo}"
 
         atags = api_get_list(f"https://api.github.com/repos/{package}/tags", pi.max_len)
@@ -83,7 +100,9 @@ def command_check(packages: list[Package], args) -> int:
         tag = tags[0]
         tag_version = trim_version_string(tag["name"])
 
-        cmp_result = compare_versions(parse_version(pi.version), parse_version(tag_version))
+        cmp_result = compare_versions(
+            parse_version(pi.version), parse_version(tag_version)
+        )
         if cmp_result < 0:
             print(f"+ {pi.repo}: {pi.version} -> {tag_version}")
             return tag_version
@@ -92,7 +111,6 @@ def command_check(packages: list[Package], args) -> int:
                 print(f"- {pi.repo}: {pi.version} != {tag_version}")
             return None
         return None
-
 
     def api_get_list(url: str, max_len: int | None = None) -> list:
         response = api_get(url)
@@ -107,18 +125,19 @@ def command_check(packages: list[Package], args) -> int:
             next_link = None
 
         if next_link:
-            sub_data = api_get_list(next_link, (max_len - len(data) if max_len else None))
+            sub_data = api_get_list(
+                next_link, (max_len - len(data) if max_len else None)
+            )
             data.extend(sub_data)
 
         return data
-
 
     def api_get(url: str):
         token = get_token()
         num_loops = 10
         wait_seconds = 60
         for _ in range(0, num_loops):
-            headers = { "Authorization": f"Bearer {token}" } if token else None
+            headers = {"Authorization": f"Bearer {token}"} if token else None
             response = get_session().get(url, headers=headers)
             if response.status_code == 403:
                 # API rate limit?
@@ -129,12 +148,10 @@ def command_check(packages: list[Package], args) -> int:
         response.raise_for_status()
         return response
 
-
     def filter_tags(tags: list, tag_filter: str) -> list:
         if not tag_filter:
             return tags
         return [t for t in tags if re.match(tag_filter, t.get("name", ""))]
-
 
     def trim_version_string(s: str) -> str:
         if not s:
@@ -144,12 +161,10 @@ def command_check(packages: list[Package], args) -> int:
                 break
         return s[idx:].strip()
 
-
-    def compare_tag_versions(a: str, b: str) -> int:
+    def compare_tag_versions(a: dict, b: dict) -> int:
         tag_a = parse_version(a["name"])
         tag_b = parse_version(b["name"])
         return compare_versions(tag_b, tag_a)
-
 
     def compare_versions(a, b) -> int:
         len_a = len(a)
@@ -160,7 +175,6 @@ def command_check(packages: list[Package], args) -> int:
                 continue
             return a[idx] - b[idx]
         return 0
-
 
     def parse_version(s: str):
         def to_int(parts, idx) -> int:
@@ -200,7 +214,7 @@ def command_check(packages: list[Package], args) -> int:
             if args.verbose:
                 print(f"checking {package.name} ...")
             else:
-                print(f"\r{idx+1:>{idxw}}/{len(packages):>{idxw}}  ", end="")
+                print(f"\r{idx + 1:>{idxw}}/{len(packages):>{idxw}}  ", end="")
             url = package.query_url
             ver = package.version if package.version else "0.0.0"
             pat = package.version_pattern
@@ -209,20 +223,30 @@ def command_check(packages: list[Package], args) -> int:
             pi.max_len = package.max_tags
             new_version = check_for_updates(pi)
             if args.update and new_version:
-                changes.append(Change(package.package_file, package.name, package.version, new_version))
+                changes.append(
+                    Change(
+                        package.package_file, package.name, package.version, new_version
+                    )
+                )
         except Exception as error:
             print(f"{package.name} failed: {error}")
 
     print("")
 
+    args_yes: bool = args.yes
     changes_to_commit: list[Change] = []
     for change in changes:
-        if args.yes:
+        if args_yes:
             pass  # auto accept change
-        elif ask_user(f"  update {change.name} from {change.old_version} to {change.new_version}?"):
-            pass  # user accepted change
         else:
-            continue
+            user_result = ask_user(f"  update {change.name} from {change.old_version} to {change.new_version}?")
+            if user_result == AskUserResult.YES:
+                pass
+            elif user_result == AskUserResult.ALL:
+                args_yes = True
+            else:
+                continue
+
         update_package(change.file, change.new_version, timestamp)
         changes_to_commit.append(change)
 
@@ -235,4 +259,6 @@ def command_check(packages: list[Package], args) -> int:
         return 1
 
     return 0
+
+
 # << command_check_code
